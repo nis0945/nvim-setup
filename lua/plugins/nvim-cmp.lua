30434569809a18a3bbb77cfb70c6b4e868939507
Hcom <72270756+nis0945@users.noreply.github.com>
2025-10-21 07:23:07 +0530

    fix: nvim-cmp file causes load errors

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -21,24 +21,34 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-c>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<Tab>"] = cmp.mapping.confirm({ select = true }, { "i", "s", "c", }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fb)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fb()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fb)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fb()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
-                { name = "nvim_lsp_signature_help" }
-            }, {
-                {
-                    name = "path",
-                    option = {
-                        trailing_slash = true,
-                    },
-                },
-            }),
+                { name = "nvim_lsp_signature_help" },
+                { name = "path" },
+                { name = "buffer" },
+            })
         }
-    end,
+    end
 }
